19e873e8f2b862f0c1d377ea3cb04ebf9f484641
f0shin <13ro921@naver.com>
2025-03-07 11:20:45 +0900

    2025-03-07

---
+++ Sol1000.py
@@ -0,0 +1,10 @@
+# 백준 1000. A + B
+
+# a = int(input())
+# b = int(input())
+# print(a + b)
+
+# map 함수 사용해서 한 줄을 읽어 들여야 함.
+
+a, b = map(int, input().split())
+print(a + b)
--- Sol27866.py
+++ Sol27866.py
@@ -1,3 +1,5 @@
+# 백준 27866. 문자와 문자열
+
 str = input()
 a = int(input())
 print(str[a - 1])
